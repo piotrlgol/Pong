f720b93177584a19a57151b8e17f0c32b247bde9
piotrlgol <piopiotrek008@gmail.com>
2020-01-28 18:59:19 +0100

    ai positions paddle to hit ball with the middle

--- pong_game/ai_analytical.py
+++ pong_game/ai_analytical.py
@@ -1,6 +1,7 @@
 class AI_analytical():
     @staticmethod
     def compute_move(paddle_pos, ball_pos, ball_vel):
+
         left_attaced = paddle_pos[0] == 20 and ball_vel[0]<0
         right_attaced = paddle_pos[0] == 670 and ball_vel[0]>0
         if(left_attaced or right_attaced):
@@ -11,8 +12,8 @@ class AI_analytical():
             while(y>490):
                 y = 980 - y
                 y = abs(y)
-            if(paddle_pos[1]-3>y):
+            if(paddle_pos[1]+45>y):
                 return 'up'
-            elif(paddle_pos[1]+3<y):
+            elif(paddle_pos[1]+55<y):
                 return 'down'
         return ' '
